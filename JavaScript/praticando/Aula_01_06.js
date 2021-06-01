4d198eb4b540b8800da2cf6a05837b2f06472113
MarcosVinicius <mviniciusrj21@gmail.com>
2021-06-01 00:38:58 -0300

    terceiro commit

---
+++ JavaScript/praticando/Aula_01_06.js
@@ -0,0 +1,37 @@
+function meses(contador){
+
+contador 
+
+if (contador == 1) {
+    console.log('Janeiro')
+    }else if(contador == 2){
+         console.log('Fevereiro')
+    }else if(contador == 3){
+        console.log('Março')
+    }else if(contador == 4){
+        console.log('Abril')
+    }else if(contador == 5){
+        console.log('Maio') 
+    }else if(contador == 6){
+        console.log('Junho') 
+    }else if(contador == 7){
+        console.log('Julho') 
+    }else if(contador == 8){
+        console.log('Agosto')
+    }else if(contador == 9){
+        console.log('Setembro')
+    }else if(contador == 10){
+        console.log('Outubro') 
+    }else if(contador == 11){
+       console.log('Novembro') 
+    }else if(contador == 12){
+       console.log('Dezembro') 
+    }else{
+       console.log("Você não digitou um numero de mes valido")
+    }         
+       
+
+}
+
+meses(21)
+
