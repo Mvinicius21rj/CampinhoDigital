06ba91f57767a5d19e36219b671a523600de3ec2
Marcos <mviniciusrj21@gmail.com>
2021-06-17 16:59:01 -0300

    aula 27

---
+++ JavaScript/praticando/aula_27.js
@@ -0,0 +1,49 @@
+//exercicio 1
+
+function simbolo(numero){
+    for(i = 1; i <= 10; i++){
+        soma = ["+"]
+        soma = soma + i
+        contagem = [0,5]
+        if(contagem < i){
+            
+            contagem.push(i)
+            final = [0,5]
+            final = contagem.push(2)
+           contagem.push(52)
+        } 
+        soma = "+"
+    
+    console.log(contagem)
+    console.log()
+    }
+   
+  
+    
+    console.log(soma)
+  }
+
+simbolo(5)
+
+A = [1]
+B = 2
+
+A.push(B)
+
+console.log(A)
+
+
+
+
+//exercicio 2
+
+
+ Array = ["javascript",1 ,"3", "Web", 20]
+
+ function filtrarNumeros(par1,par2,par3,par4,par5){
+  tipo = typeof(par1,par2,par3,par4,par5) 
+  return tipo    
+
+ }
+
+ console.log(filtrarNumeros(8,"dkfj"))
